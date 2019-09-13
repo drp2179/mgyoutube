c7fffc245c609e1820064e31b7bc5f7412913f33
Daniel R Pedersen <drp2179@gmail.com>
2019-09-12 19:21:15 -1000

    fe-vue-parents implementation of story 10

--- mgyoutube-multitech/fe-vue-parents/api.parents.functions.js
+++ mgyoutube-multitech/fe-vue-parents/api.parents.functions.js
@@ -33,12 +33,6 @@ function api_getAllChildrenForParent(parentUsername, successCallback, failureCal
         }
     }
     httpRequest.onloadend = httpRequest.onReadyStateChange;
-    // httpRequest.onloadend = (e) => {
-    //     console.warn("/api/auth onloadend e: " + JSON.stringify(e));
-    //     console.warn("/api/auth onloadend httpRequest: " + JSON.stringify(httpRequest));
-    //     console.warn("/api/auth onloadend httpRequest.readyState: " + httpRequest.readyState)
-    //     console.warn("/api/auth onloadend httpRequest.status: " + httpRequest.status)
-    // }
 
     try {
         httpRequest.open("GET", url, true);
@@ -93,12 +87,6 @@ function api_addChildToParent(parentUsername, childUsername, childPassword, succ
         }
     }
     httpRequest.onloadend = httpRequest.onReadyStateChange;
-    // httpRequest.onloadend = (e) => {
-    //     console.warn("/api/auth onloadend e: " + JSON.stringify(e));
-    //     console.warn("/api/auth onloadend httpRequest: " + JSON.stringify(httpRequest));
-    //     console.warn("/api/auth onloadend httpRequest.readyState: " + httpRequest.readyState)
-    //     console.warn("/api/auth onloadend httpRequest.status: " + httpRequest.status)
-    // }
 
     const payloadObj = {}
     payloadObj.username = childUsername;
@@ -124,3 +112,106 @@ function api_addChildToParent(parentUsername, childUsername, childPassword, succ
 
 }
 
+
+function api_saveSearch(parentUsername, searchPhrase, successCallback, failureCallback) {
+    const url = '/api/parents/' + parentUsername + '/searches/' + searchPhrase;
+
+    const httpRequest = new XMLHttpRequest();
+    httpRequest.onReadyStateChange = function () {
+        console.warn(url, "onReadyStateChange");
+
+        if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.status === 201) {
+                if (successCallback) {
+                    console.log("should be calling success callback with");
+                    successCallback();
+                }
+                else {
+                    console.log("status(" + this.status + ") == 201 and success callback not defined");
+                }
+            }
+            else if (failureCallback) {
+                console.log("should be calling failure callback");
+                failureCallback();
+            }
+            else {
+                console.log("status(" + this.status + ") != 200 and failure callback not defined");
+            }
+        }
+    }
+    httpRequest.onError = (e) => {
+        console.warn(url, JSON.stringify(e));
+        if (failureCallback) {
+            failureCallback();
+        }
+    }
+    httpRequest.onloadend = httpRequest.onReadyStateChange;
+
+    try {
+        httpRequest.open("PUT", url, true);
+        console.log(url, "PRE-PUT")
+        httpRequest.send();
+    }
+    catch (err) {
+        console.warn(url, err, JSON.stringify(err));
+        if (failureCallback) {
+            failureCallback();
+        }
+    }
+    finally {
+        console.log(url, "POST-PUT")
+    }
+}
+
+
+function api_getSavedSearchesForParent(parentUsername, successCallback, failureCallback) {
+    const url = '/api/parents/' + parentUsername + '/searches';
+
+    const httpRequest = new XMLHttpRequest();
+    httpRequest.onReadyStateChange = function () {
+        console.warn(url, "onReadyStateChange");
+
+        if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.status === 200) {
+                const searches = JSON.parse(this.response)
+
+                if (successCallback) {
+                    console.log("should be calling success callback with", searches);
+                    successCallback(searches);
+                }
+                else {
+                    console.log("status(" + this.status + ") == 200 and success callback not defined");
+                }
+            }
+            else if (failureCallback) {
+                console.log("should be calling failure callback");
+                failureCallback();
+            }
+            else {
+                console.log("status(" + this.status + ") != 201 and failure callback not defined");
+            }
+        }
+    }
+    httpRequest.onError = (e) => {
+        console.warn(url, JSON.stringify(e));
+        if (failureCallback) {
+            failureCallback();
+        }
+    }
+    httpRequest.onloadend = httpRequest.onReadyStateChange;
+
+    try {
+        httpRequest.open("GET", url, true);
+        console.log(url, "PRE-GET")
+        httpRequest.send();
+    }
+    catch (err) {
+        console.warn(url, err, JSON.stringify(err));
+        if (failureCallback) {
+            failureCallback();
+        }
+    }
+    finally {
+        console.log(url, "POST-GET")
+    }
+}
