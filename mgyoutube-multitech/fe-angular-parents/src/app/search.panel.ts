40ce6c18b33ddabddeaffdaac847eccc4715e73c
Daniel R Pedersen <drp2179@gmail.com>
2019-09-13 20:41:42 -1000

    fe-angular-parents now supports story 10

--- mgyoutube-multitech/fe-angular-parents/src/app/search.panel.ts
+++ mgyoutube-multitech/fe-angular-parents/src/app/search.panel.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { globalState } from "./globalstate"
 declare var api_getVideoSearchResults: any;
+declare var api_saveSearch: any;
+declare var api_getSavedSearchesForParent: any;
 
 @Component({
     selector: 'search-panel',
     template: `
         <div id="searchpanel">
+            <div id="savedsearchespanel">
+                <div *ngFor="let phrase of this.theSavedSearches; let i = index">
+                    <div>{{phrase}}</div>
+                </div>
+            </div>
             <player-panel [videoId]="theVideoId"></player-panel>
             <div id="searchtermspanel">
                 <h3>Search Terms</h3>
@@ -13,6 +21,7 @@ declare var api_getVideoSearchResults: any;
                 <button id="submitSearchTermsButton" (click)="onSearchHandler()">Search</button>
             </div>
             <div id="searchresultspanel">
+                <div><button id="submitSaveSearchButton" (click)="onSaveSearchHandler()">Save Search</button></div>
                 <div *ngFor="let video of this.theVideos; let i = index">
                     <div key={i} id="search-result-{{i}}">
                         <div id="search-result-{{i}}-video-{{video.videoId}}"><a id="search-result-{{i}}-watch" (click)="watchVideo(video.videoId)" href="#"><img src="{{video.thumbnailUrl}}"/>{{video.title}}</a></div> 
@@ -28,10 +37,34 @@ export class SearchPanel {
     theVideoId: string;
     theSearchTerms: string;
     theVideos = [];
+    theSavedSearches = [];
 
     constructor() {
         this.theVideoId = "";
         this.theSearchTerms = "";
+    }
+
+    onSaveSearchHandler(event: any) {
+        if (this.theSearchTerms) {
+            api_saveSearch(globalState.loggedInUsername, this.theSearchTerms, () => this.saveSearchSuccess(), () => this.saveSearchFailed());
+        }
+        else {
+            alert("no search phrase to save")
+        }
+    }
+
+    saveSearchSuccess() {
+        api_getSavedSearchesForParent(globalState.loggedInUsername, (savedSearches: any) => this.getSavedSearchesSuccess(savedSearches), () => this.getSavedSearchesFailed());
+    }
+    saveSearchFailed() {
+        alert("saving search for  '" + this.theSearchTerms + "' failed");
+    }
+
+    getSavedSearchesSuccess(savedSearches: any) {
+        this.theSavedSearches = savedSearches;
+    }
+    getSavedSearchesFailed() {
+        alert("retrieving saved searches failed");
     }
 
     onSearchHandler(event: any) {
@@ -43,7 +76,6 @@ export class SearchPanel {
     }
 
     searchSuccess(videos: any) {
-        this.theSearchTerms = null;
         this.theVideos = videos;
     }
 
@@ -55,4 +87,10 @@ export class SearchPanel {
         console.log("watchVideo, " + event);
         this.theVideoId = event;
     }
+
+    ngOnInit() {
+        console.log("searchpanel.ngOnInit");
+        api_getSavedSearchesForParent(globalState.loggedInUsername, (savedSearches: any) => this.getSavedSearchesSuccess(savedSearches), () => this.getSavedSearchesFailed());
+    }
+
 }
